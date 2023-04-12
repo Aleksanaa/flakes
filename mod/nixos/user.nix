29d33b75e7fff879ee3324cc16afa9d889115da3
aleksana <me@aleksana.moe>
2023-04-12 17:22:55 +0800

    force user id

--- mod/nixos/user.nix
+++ mod/nixos/user.nix
@@ -7,6 +7,8 @@
       ${userName} = {
         isNormalUser = true;
         extraGroups = [ "wheel" "nix-config" ];
+        # force user id to allow modifying /run directory
+        uid = 1000;
       };
       root.extraGroups = [ "nix-config" ];
     };
